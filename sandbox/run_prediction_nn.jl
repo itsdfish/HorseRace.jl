4546ca7b3715edda7ec4f90a8dd107e80fa4c024
itsdfish <itsdfish@gmail.com>
2023-12-29 06:50:55 -0500

    add nn

--- sandbox/run_prediction_nn.jl
+++ sandbox/run_prediction_nn.jl
@@ -6,14 +6,14 @@ using Pkg
 Pkg.activate("..")
 using DataFrames
 using Distributions
+using Flux 
 using HorseRace
-using GLM
 using Random
 using Revise
 using Plots 
 
 function collect_data(game, player)
-    x = fill(0.0, 6)
+    x = zeros(Float32, 6)
     for i ∈ 2:7
         j = 14 - i  
         x[i-1] += sum((i .== player.cards) .|| (j .== player.cards))
@@ -41,13 +41,35 @@ end
 ###########################################################################################################
 Random.seed!(5202)
 n_players = 4
-outcome = map(_ -> run_sim(;n_players), 1:10_000)
+outcome = map(_ -> run_sim(;n_players), 1:100_000)
 outcome = vcat(outcome...)
-outcome = reduce(vcat, transpose.(outcome))
-df = DataFrame(outcome, :auto)
-cols = [string("h$h") for h ∈ 2:7]
-push!(cols, "payoff")
-rename!(df, cols)
+outcome = hcat(outcome...)
+train_x = outcome[1:6,:]
+train_y = outcome[end,:]'
+train_data = Flux.Data.DataLoader((train_x, train_y), batchsize=5_000)
+###################################################################################################
+#                                        Create Network
+###################################################################################################
+# 6 nodes in input layer, 3 hidden layers, 1 node for output layer
+model = Chain(
+    Dense(6, 5, tanh),
+    Dense(5, 5, tanh),
+    Dense(5, 1, identity)
+)
 
-model = @formula(payoff ~ h2 + h3 + h4 + h5 + h6 + h7)
-ols = lm(model, df)
+# loss function
+loss_fn(a, b) = Flux.huber_loss(model(a), b) 
+
+# optimization algorithm 
+opt = ADAM(0.001)
+
+n_epochs = 100
+train_loss = zeros(n_epochs)
+test_loss = zeros(n_epochs)
+
+for i ∈ 1:n_epochs
+    Flux.train!(loss_fn, Flux.params(model), train_data, opt)
+    train_loss[i] = loss_fn(train_x, train_y)
+end
+
+plot(train_loss)
